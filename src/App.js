b9639916a237020d784785529a64c3abaef543cf
yuruse419 <chaseorion19@gmail.com>
2024-01-09 15:04:18 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+        <p>Chase Shertzer</p>
+        <p>CSC 435 - Advanced Web App Development</p>
+        <p>Assignment 1</p>
+        <p>1/9/2024</p>
+    </main>
   );
 }
 
